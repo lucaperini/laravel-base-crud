cda8b56544abe7868bfb78149a75041d13bcae28
lucaperini <luca.perini@gmail.com>
2022-05-17 15:32:08 +0200

    create comics table in db laravel_base_crud

--- laravel-base-crud/database/migrations/2022_05_17_130734_create_comics_table.php
+++ laravel-base-crud/database/migrations/2022_05_17_130734_create_comics_table.php
@@ -15,6 +15,13 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->text('description');
+            $table->text('thumb');
+            $table->decimal('price', 6,2);
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type');
             $table->timestamps();
         });
     }
