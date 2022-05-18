c5764cb075b773d84a8f6fad6e555e741fec2f11
lucaperini <luca.perini@gmail.com>
2022-05-18 09:03:57 +0200

    fix

--- laravel-base-crud/database/seeds/ComicsTableSeeder.php
+++ laravel-base-crud/database/seeds/ComicsTableSeeder.php
@@ -14,13 +14,13 @@ class ComicsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for ($i=0; $i < 50; $i++) {
+        for ($i=0; $i < 200; $i++) {
 
         $newComic = new Comic();
         $newComic->title = $faker->word();
         $newComic->description = $faker->paragraph(10);
         $newComic->thumb = $faker->imageUrl(360, 360, 'animals', true, 'dogs');
-        $newComic->price = $faker->randomFloat(2);
+        $newComic->price = $faker->randomFloat(1, 20, 30);
         $newComic->series =$faker->word();
         $newComic->sale_date =$faker->date();
         $newComic->type =$faker->word();
